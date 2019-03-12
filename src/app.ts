a85218d466cadfd4c6ce1eef435fe041f719f6a5
xroah <396069583@qq.com>
2019-03-12 13:28:32 +0800

    log4js

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,12 @@ import session from "express-session";
 import connectRedis from "connect-redis";
 import admin from "./admin";
 import publicRouter from "./routes/public";
+import log4js from "log4js";
 
 const app = express();
 const RedisStore = connectRedis(session);
+const logger = log4js.getLogger("app");
+logger.level = "debug";
 
 app.use(session({
     secret: "my_blog",
@@ -27,6 +30,11 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
+
+app.all("*", (req, res, next) => {
+    logger.debug(`method: ${req.method}, url: ${req.url}, params: ${JSON.stringify(req.body)}`);
+    next();
+});
 
 app.use("/api/admin", admin);
 
