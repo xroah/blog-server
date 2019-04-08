62f4cd98f6ca05c18daa6926a50fbdd2942cb5ca
xroah <396069583@qq.com>
2019-04-08 18:37:43 +0800

    remove event emitter

--- src/common/request.ts
+++ src/common/request.ts
@@ -1,8 +1,5 @@
 import http, { ClientRequestArgs } from "http";
 import https from "https";
-import EventEmitter from "events";
-
-export const reqEmitter = new EventEmitter();
 
 export default function request(isHttps?: boolean, opt: ClientRequestArgs = {}) {
     let _request = isHttps ? https.request : http.request;
@@ -11,18 +8,15 @@ export default function request(isHttps?: boolean, opt: ClientRequestArgs = {}) 
             let buffer = Buffer.allocUnsafe(0);
             res.on("end", function () {
                 resolve(buffer);
-                reqEmitter.emit("end", buffer);
             });
 
             res.on("data", chunk => {
                 buffer = Buffer.concat([buffer, chunk], buffer.length + chunk.length);
-                reqEmitter.emit("data", chunk);
             });
         });
 
         req.on("error", err => {
             reject(err);
-            reqEmitter.emit("error", err);
         });
         req.end();
     });
