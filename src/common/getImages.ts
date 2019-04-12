26c82f5af2edb3b9f2787f33635e8002f7fb54dd
xroah <396069583@qq.com>
2019-04-12 18:06:48 +0800

    image sort

--- src/common/getImages.ts
+++ src/common/getImages.ts
@@ -3,13 +3,13 @@ import {
     Response,
     NextFunction
 } from "express";
-import {ObjectID} from "mongodb";
-import {find} from "../db";
-import {response} from "../common";
+import { ObjectID } from "mongodb";
+import { find } from "../db";
+import { response } from "../common";
 
 export default async function getImages(req: Request, res: Response, next: NextFunction) {
     let {
-        query: {albumId}
+        query: { albumId }
     } = req;
     try {
         if (albumId == 1 || albumId == 2) {
@@ -17,7 +17,15 @@ export default async function getImages(req: Request, res: Response, next: NextF
         } else {
             albumId = new ObjectID(albumId);
         }
-        let ret = await find("resources", {albumId}).toArray();
+        let ret = await find(
+            "resources",
+            { albumId },
+            {
+                sort: {
+                    createTime: -1
+                }
+            }
+        ).toArray();
         response(res, 0, ret);
     } catch (err) {
         next(err);
