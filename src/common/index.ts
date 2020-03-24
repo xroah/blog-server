c33b0b9135ac66d842a487ce4916e222c3a16bc3
xroah <396069583@qq.com>
2020-03-24 13:00:20 +0800

    remove all for code refactoring

--- src/common/index.ts
+++
@@ -1,6 +0,0 @@
-export {default as getArticles} from "./getArticles";
-export {default as response} from "./response";
-export {default as request} from "./request";
-export {default as getAlbums} from "./getAlbums";
-export {default as getImages} from "./getImages";
-export {default as handleAlbumId} from "./handleAlbumId";
--- src/db/collections.ts
+++
@@ -1,7 +0,0 @@
-export const ARTICLES = "articles";
-export const CLASSIFICATIONS = "classifications";
-export const COMMENTS = "comments";
-export const ALBUMS = "albums";
-export const USERS = "users";
-export const RESOURCES = "resources";
-export const VERSIONS = "versions";
