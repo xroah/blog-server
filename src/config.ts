d99b033c20e9702abb9e554245e4d530b1a04af6
xroah <396069583@qq.com>
2020-05-08 21:47:40 +0800

    init

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const PORT = 3000;
+export const DB_USER = "";
+export const DB_PWD = "";
+export const DB_NAME = "blog";
+export const DB_URL = "mongodb://localhost:27017";
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-function test(a: number, b: number) {
-    const sum = a + b;
+import express from "express";
+import { 
+    PORT,
+    DB_NAME,
+    DB_URL
+ } from "./config";
+import { MongoClient } from "mongodb";
 
-    return sum;
-}
+const app = express();
 
-test(5, 6);
+MongoClient.connect(DB_URL, (err, client) => {
+    if (err) throw err;
+
+    const db = client.db(DB_NAME);
+
+    db.collection("users").find({}).toArray().then(res => {
+        console.log(res);
+    }).catch(err => {
+        console.log(err)
+    });
+
+    app.get("/", (req, res) => {
+        res.send("Hello world!");
+    });
+
+    app.listen(PORT);
+});
+
