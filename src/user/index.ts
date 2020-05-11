bdbee323006b57dc0add8f5af3548fd08b318ee0
xroah <396069583@qq.com>
2020-05-11 20:51:51 +0800

    add fetch bing picture api

--- src/user/index.ts
+++ src/user/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import fetchBingPic from "./controllers/fetchBingPic";
 import nonMatch from "../common/controllers/nonMatch";
 
 const app = express();
@@ -6,5 +7,6 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Hello world!");
 });
+app.get("/bg", fetchBingPic);
 
 export default app;
