f9aabdeb5d6ee4233d5001b96ccec8a0c123c800
xroah <396069583@qq.com>
2020-05-12 21:48:55 +0800

    login/logout

---
+++ src/admin/controllers/login.ts
@@ -0,0 +1,79 @@
+import {
+    Request,
+    Response,
+    NextFunction
+} from "express";
+import { findOne } from "../../db";
+import { createHash } from "crypto";
+
+function md5(str: string) {
+    return createHash("md5")
+        .update(str, "utf8")
+        .digest("hex");
+}
+
+export function login(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    let {
+        username,
+        password
+    } = req.body || {};
+
+    if (!username || !password) {
+        return res.json({
+            code: -2,
+            msg: "请输入用户名和密码！"
+        });
+    }
+
+    findOne(
+        "users",
+        {
+            username,
+            password: md5(password)
+        },
+        {
+            projection: {
+                _id: 0,
+                password: 0
+            }
+        }).then(ret => {
+            if (!ret) {
+                return res.json({
+                    code: -1,
+                    msg: "用户名或密码错误！"
+                });
+            }
+
+            const session = req.session || {} as any;
+
+            session.role = ret.role;
+            session.username = username;
+
+            return res.json({
+                code: 0,
+                msg: "登录成功!",
+                data: ret
+            });
+        }).catch(next);
+}
+
+export function logout(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    req.session?.destroy(err => {
+        if (err) {
+            return next(err);
+        }
+
+        res.json({
+            code: 0,
+            msg: "退出成功"
+        });
+    });
+}
