8a27343d158c641b411b18e110f4c21e4e6f1971
xroah <396069583@qq.com>
2020-05-13 13:54:30 +0800

    start

---
+++ startDev.js
@@ -0,0 +1,18 @@
+const childProc = require("child_process");
+
+process.env.NODE_ENV = "development";
+
+childProc.spawn(
+    "nodemon",
+    [
+        "-e",
+        "ts",
+        "--exec",
+        "ts-node",
+        "."
+    ],
+    {
+        stdio: "inherit",
+        shell: true
+    }
+);
