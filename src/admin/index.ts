0e48aae53b1cec77d1c9ba1955da513065985285
xroah <396069583@qq.com>
2020-05-24 11:35:12 +0800

    revise upload url

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -8,6 +8,6 @@ const app = express();
 app.all("*", interceptor);
 app.post("/login", login);
 app.post("/logout", logout);
-app.post("/fileUpload", upload);
+app.post("/upload", upload);
 
 export default app;
